7d8757fa3d941f298e0653108f74a618a1288013
Daniel y Laura <laurasofiamoriano@gmail.com>
2024-10-19 20:20:15 -0500

    Modelo Organizador

---
+++ app/Models/Organizador.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Organizador extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'apellido', 'email', 'telefono'];
+
+    public function eventos()
+    {
+        return $this->belongsToMany(Evento::class, 'participaciones')
+                    ->withPivot('rol')
+                    ->withTimestamps();
+    }
+}
