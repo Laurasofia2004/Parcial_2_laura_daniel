2046cf343d62e09c294330613d7bca54807b2cda
Daniel y Laura <laurasofiamoriano@gmail.com>
2024-10-19 20:49:03 -0500

    Create Organizadores

---
+++ app/View/Components/Organizadores/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Crear Organizador</h1>
+    <form action="{{ route('organizadores.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="apellido">Apellido</label>
+            <input type="text" name="apellido" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="telefono">Teléfono</label>
+            <input type="text" name="telefono" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-success">Guardar</button>
+    </form>
+</div>
+@endsection
