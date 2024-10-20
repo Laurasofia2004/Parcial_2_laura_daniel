45fab651a710dec8b542f902f053906772c2e5d7
Laura Benavides <laurasofiamoriano@gmail.com>
2024-10-20 16:30:12 -0500

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\OrganizadorController;
+use App\Http\Controllers\ParticipacionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +18,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rutas para Eventos
+    Route::resource('eventos', EventoController::class);
+
+    // Rutas para Organizadores
+    Route::resource('organizadores', OrganizadorController::class);
+
+    // Rutas para Participaciones
+    Route::resource('participaciones', ParticipacionController::class);
 });
 
-require __DIR__.'/auth.php';
+require _DIR_.'/auth.php';
